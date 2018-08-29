d8f944801efaa6fc653bf24b835ec5fc38dadbac
Youness Houdass <youness.h@hiitconsulting.com>
2018-08-29 16:29:58 +0100

    Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { DropdownDirective } from './dropdown.directive';
 import { ComponentsModule } from './components/components.module';
 import { WildcardRoutingModule } from './wildcard-routing/wildcard-routing.module';
 import { DirectivesModule } from './directives/directives.module';
-import {FirebaseModule} from "./firebase/firebase.module";
+import {FirebaseModule} from './firebase/firebase.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ItemComponent, DropdownDirective],
--- src/app/firebase/firebase-routing.module.ts
+++ src/app/firebase/firebase-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {FirebaseComponent} from "./firebase.component";
-
+import {FirebaseComponent} from './firebase.component';
 
 const firebaseRoutes: Routes = [
   {
