d7334c51cd09ec38d170ac907191deefb250266a
Houdass Youness <houd.youness@gmail.com>
2018-09-01 21:30:26 +0100

    Add anchors

--- src/app/animations/transition/transition.component.ts
+++ src/app/animations/transition/transition.component.ts
@@ -13,12 +13,14 @@ export class TransitionComponent {
   transitionTs1: string;
   transitionTs2: string;
   transitionTs3: string;
+  transitionTs4: string;
   courses: string[];
   constructor(utilService: UtilService) {
     this.courses = ['Course 1', 'Course 2', 'Course 3'];
     this.transitionTs1 = utilService.typescript(markdown.transitionTs1);
     this.transitionTs2 = utilService.typescript(markdown.transitionTs2);
     this.transitionTs3 = utilService.typescript(markdown.transitionTs3);
+    this.transitionTs4 = utilService.typescript(markdown.transitionTs4);
   }
 
   addCourse(course: HTMLInputElement): void {
