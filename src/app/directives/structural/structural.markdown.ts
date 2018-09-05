664cca3ee2a385914750a0798286b044635c709d
Youness Houdass <youness.h@hiitconsulting.com>
2018-09-05 15:20:48 +0100

    ngFor fix

--- src/app/directives/structural/structural.markdown.ts
+++ src/app/directives/structural/structural.markdown.ts
@@ -4,6 +4,6 @@ export const markdown: any = {
   ngIfConsistent: `<p *ngIf:"isVisible, then condition else noCondition"></p>
 <ng-template #condition><p>My message if true<p></ng-template>
 <ng-template #noCondition><p>My message if false<p></ng-template>`,
-  ngFor: `<p *ngFor:"let item of items, let i : index">{{ i }} - {{ item.name }}</p>`,
+  ngFor: `<p *ngFor:"let item of items, let i = index">{{ i }} - {{ item.name }}</p>`,
   ngForAs: `<p *ngFor:"let item of items, index as i">{{ i }} - {{ item.name }}</p>`
 };
