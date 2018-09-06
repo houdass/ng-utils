f53dc13fc85df910a40868c145613c10b05858c3
Houdass Youness <houd.youness@gmail.com>
2018-09-06 22:17:00 +0100

    Store

---
+++ src/app/store/store.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreComponent } from './store.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [StoreComponent]
+})
+export class StoreModule { }
